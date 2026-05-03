8e1b824cfcfa3ffadd51ba7944044bbea1222c3b
goerkemguenyil <g@guenyil.de>
2026-05-03 11:24:53 +0200

    css

--- views/browse.js
+++ views/browse.js
@@ -21,7 +21,7 @@ function initBrowseModule() {
                 <svg class="browse-search-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                 <input type="search" class="browse-search-input" placeholder="Suchen">
             </div>
-            <h2 class="browse-title">Speise- & Genusskarte</h2>
+            <h2 class="browse-title">Kategorien</h2>
             <div class="browse-grid">
                 ${cardsHtml}
             </div>
